131a9718f42a8a51ec92e914f3988f4d321533e9
divyam110903 <117055743+divyam110903@users.noreply.github.com>
2024-04-25 00:08:13 +0530

    Update task.js

--- task.js
+++ task.js
@@ -1,29 +1,15 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+let tasks = [];
 
 function addTask() {
   if (inputBox.value === '') {
     alert("You must add something!");
   } else {
-    fetch('/tasks', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ text: inputBox.value })
-    })
-      .then(response => response.json())
-      .then(data => {
-        let li = document.createElement('li');
-        li.innerHTML = data.text;
-        li.dataset.id = data._id; 
-        listContainer.appendChild(li);
-        let span = document.createElement('span');
-        span.innerHTML = "X";
-        li.appendChild(span);
-        saveData(); 
-      })
-      .catch(error => console.error(error));
+    const newTask = { text: inputBox.value, _id: Date.now() }; // Simulated _id with timestamp
+    tasks.push(newTask);
+    renderTask(newTask);
+    saveData();
     inputBox.value = "";
   }
 }
@@ -33,42 +19,31 @@ listContainer.addEventListener('click', function (e) {
     e.target.classList.toggle("checked");
   } else if (e.target.tagName === "SPAN") {
     const taskId = e.target.parentElement.dataset.id;
-    fetch(`/tasks/${taskId}`, {
-      method: 'DELETE'
-    })
-      .then(() => {
-        e.target.parentElement.remove();
-        saveData();
-      })
-      .catch(error => console.error(error));
+    tasks = tasks.filter(task => task._id != taskId);
+    e.target.parentElement.remove();
+    saveData();
   }
 }, false);
 
+function renderTask(task) {
+  let li = document.createElement('li');
+  li.innerHTML = task.text;
+  li.dataset.id = task._id; 
+  listContainer.appendChild(li);
+  let span = document.createElement('span');
+  span.innerHTML = "X";
+  li.appendChild(span);
+}
+
 function saveData() {
-  localStorage.setItem("tasks", listContainer.innerHTML);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 function showTask() {
   const savedTasks = localStorage.getItem("tasks");
   if (savedTasks) {
-    listContainer.innerHTML = savedTasks;
-  } else {
-    fetch('/tasks')
-      .then(response => response.json())
-      .then(data => {
-        listContainer.innerHTML = '';
-        data.forEach(task => {
-          let li = document.createElement('li');
-          li.innerHTML = task.text;
-          li.dataset.id = task._id;
-          listContainer.appendChild(li);
-          let span = document.createElement('span');
-          span.innerHTML = "\u00d7";
-          li.appendChild(span);
-        });
-        saveData(); 
-      })
-      .catch(error => console.error(error));
+    tasks = JSON.parse(savedTasks);
+    tasks.forEach(task => renderTask(task));
   }
 }
 
